53ec156074bc62f963ace71367826499d0dae43c
Kevin Barton <kbarton@tamusa.edu>
2022-05-13 16:49:50 -0500

    Update destroy.sh

--- router_basic/lab2_configuration/destroy.sh
+++ router_basic/lab2_configuration/destroy.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-br1="clab-br1"
-br2="clab-br2"
-f=lab1.yml
+l=lab1
+f=$l.yml
 
 # Deploy the clab topology
 sudo clab destroy --topo $f
 
 # Remove containerlab topo file
 sudo rm $f
-
-# Clean up bridges
-sudo ip link set $br1 down
-sudo ip link set $br2 down
-sudo brctl delbr $br1
-sudo brctl delbr $br2
