c3b705c94b08ac41329dbdec17e608599ddbdb72
Kevin Barton <kbarton@tamusa.edu>
2022-07-16 16:27:37 -0600

    Update deploy.sh

--- network_services/lab1_practice/deploy.sh
+++ network_services/lab1_practice/deploy.sh
@@ -83,6 +83,7 @@ conf="/opt/vyatta/etc/config/config.boot"
 gw=172.20.0.1
 
 # Install net-tools on web
+$d exec -it $w apt update
 $d exec -it $w apt install net-tools -y
 
 $d exec -it $dns1 ip addr add $a1 $b1
