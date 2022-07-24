6880b550ba8530bb24b0c9243593ce7052db6b90
Kevin Barton <kbarton@tamusa.edu>
2022-07-24 08:52:17 -0500

    Update deploy.sh

--- network_discovery/lab1_practice/deploy.sh
+++ network_discovery/lab1_practice/deploy.sh
@@ -76,10 +76,10 @@ a4=10.100.1.65/24
 a5=10.150.1.99/24
 b1="dev eth1"
 h1="clab-$l-h1"
-h2="clab-$l-h2"
-w="clab-$l-web"
-d="clab-$l-dns1"
-f="clab-$l-ftp"
+t1="clab-$l-h2"
+w="clab-$l-zue"
+d="clab-$l-zan"
+f="clab-$l-zin"
 r1="clab-$l-r1"
 r2="clab-$l-r2"
 r3="clab-$l-r3"
@@ -87,39 +87,22 @@ conf="/opt/vyatta/etc/config/config.boot"
 gw=172.20.0.1
 
 # Configure static IP addresses
-$d exec -it $dns1 ip addr add $a1 $b1
-$d exec -it $dns2 ip addr add $a2 $b1
+$d exec -it $f ip addr add $a1 $b1
+$d exec -it $d ip addr add $a2 $b1
 $d exec -it $w ip addr add $a3 $b1
 $d exec -it $h1 ip addr add $a4 dev eth2
+$d exec -it $t1 ip addr add $a5 $b1
 
 # Configure default gateways on hosts
 $d exec -it $h1 route add default gw 10.100.1.1 eth2
-$d exec -it $dns1 route add default gw 10.200.1.1 eth1
-$d exec -it $dns2 route add default gw 10.200.1.1 eth1
+$d exec -it $d route add default gw 10.200.1.1 eth1
+$d exec -it $f route add default gw 10.200.1.1 eth1
 $d exec -it $w route add default gw 10.200.1.1 eth1
-
-# Delete Docker default gateways
-# $d exec -it $h1 route delete default gw $gw eth0
-$d exec -it $h2 route delete default gw $gw eth0
-$d exec -it $w route delete default gw $gw eth0
-
-# Create /config directory on r1 and r2 to start dhcp servers
-$d exec -it $r1 mkdir /config
-$d exec -it $r2 mkdir /config
 
 # Copy router config files
 $d cp r1.config.boot $r1:$conf
 $d cp r2.config.boot $r2:$conf
 $d cp r3.config.boot $r3:$conf
-
-# Prohibit password login for ssh
-$d exec -it $h1 sed -i 's/#PasswordAuthentication no/PasswordAuthentication no/g' /etc/ssh/sshd_config && \
-    sed -i 's/#UsePAM yes/UsePAM yes/g' /etc/ssh/sshd_config && \
-    sed -i 's/#PrintMotd yes/PrintMotd yes/g' /etc/ssh/sshd_config
-
-# Create ssh host keys and start ssh server
-$d exec -it $h1 ssh-keygen -A
-$d exec -it $h1 /etc/init.d/ssh start
 
 printf "Wait 120 seconds to reboot routers\n"
 sleep 30
